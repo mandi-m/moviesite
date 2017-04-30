71807c47a3f1cae3551f84bfb21fc29aec956976
mandi-m <mmeidlinger@yahoo.com>
2017-04-30 16:53:27 -0400

    back to basic search - deleted all other sliders

--- app/components/AsyncSearch.jsx
+++ app/components/AsyncSearch.jsx
@@ -1,28 +1,14 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import {asyncContainer, Typeahead} from 'react-bootstrap-typeahead'
-//import SliderComponent from './Slider'
-import CustomizedSlider from './CustomizedSlider'
-import CustomizedRange from './CustomizedRange'
 const AsyncTypeahead = asyncContainer(Typeahead)
-import handle from './handle'
 import BasicSlider from './BasicSlider'
-
-const Tooltip = require('rc-tooltip')
-const Slider = require('rc-slider')
-const createSliderWithTooltip = Slider.createSliderWithTooltip
-const Handle = Slider.Handle
-const Range = createSliderWithTooltip(Slider.Range)
-
-
-// require('rc-slider/assets/index.css')
-// require('rc-tooltip/assets/bootstrap.css')
 
 const AsyncSearch = React.createClass({
   getInitialState() {
     return {
-      allowNew: false,
-      multiple: false,
+      // allowNew: false,
+      // multiple: false,
       options: [],
     }
   },
@@ -32,7 +18,7 @@ const AsyncSearch = React.createClass({
     const style = { width: 400, margin: 50 }
     return (
       <div>
-                <pre>{JSON.stringify(this.state, null, 2)} </pre>
+        {/*<pre>{JSON.stringify(this.state, null, 2)} </pre>*/}
         <AsyncTypeahead
           {...this.state}
           labelKey="original_title"
@@ -46,20 +32,6 @@ const AsyncSearch = React.createClass({
           <p>Range with custom handle</p>
           <Range min={0} max={20} defaultValue={[3, 10]} tipFormatter={value => `${value}%`} />
         </div>*/}
-        <div style={style}>
-          <p>Customized Slider</p>
-          {/*<CustomizedSlider />*/}
-          <Slider tipTransitionName="rc-slider-tooltip-zoom-down" />
-        </div>
-    <div style={style}>
-      <p>Basic Range，`allowCross=false`</p>
-      <Range allowCross={false} defaultValue={[1888, 2017]} />
-    </div>
-
-        <div style={wrapperStyle}>
-      <p>Range with custom handle</p>
-      <Range min={1888} max={2017} defaultValue={[1950, 2010]} tipFormatter={value => `${value}year`} />
-    </div>
     <div style={style}>
       <BasicSlider />
     </div>
@@ -122,16 +94,18 @@ const AsyncSearch = React.createClass({
     if (!query) {
       return
     }
-
+    //console.log(this.props.endSearchDate.substr(0, 4))
     fetch(`https://api.themoviedb.org/3/search/movie?query=${query}&api_key=ca66037b5b40d478784f66cc2a04b448`)
-    // fetch(`https://api.themoviedb.org/3/discover/movie?api_key=ca66037b5b40d478784f66cc2a04b448&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&release_date.gte=2017-01-01&release_date.lte=2017-12-31`)
+    // fetch(`https://api.themoviedb.org/3/discover/movie?api_key=ca66037b5b40d478784f66cc2a04b448&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=true&page=1&release_date.gte=2015-01-01&release_date.lte=2017-12-31`)
       .then(resp => resp.json())
-      .then(json => {
-        return json.results.filter(function(x) {
-          return x.release_date.indexOf('2017')
-        })
-      })
-      .then(jsonfiltered => this.setState({options: jsonfiltered}))
+      .then(jsonfiltered => this.setState({options: jsonfiltered.results}))
+
+
+      // .then(json => {
+      //   return json.results.filter(function(x) {
+      //     return x.release_date.indexOf(this.props.endSearchDate.substr(0, 4)) >= 0
+      //   })
+      // })
   },
 })
 
@@ -142,7 +116,9 @@ import { setSelectedMovieId } from '../reducers/movies'
 
 const mapStateToProps = (state, ownProps) => (
   {
-    selectedMovieId: state.movies.selectedMovieId
+    selectedMovieId: state.movies.selectedMovieId,
+    startSearchDate: state.movies.startSearchDate,
+    endSearchDate: state.movies.endSearchDate
   }
 )
 
--- app/components/BasicSlider.jsx
+++ app/components/BasicSlider.jsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { connect } from 'react-redux'
 import InputRange from 'react-input-range';
 
-export default class BasicSlider extends React.Component {
+class BasicSlider extends React.Component {
   constructor(props) {
-    super(props);
-
+    super(props)
     this.state = {
       value: { min: 1950, max: 2010 },
-    };
+    }
+  }
+
+  handleChange(valueObject) {
+    this.props.setStartDate(valueObject.value.min + '-01-01')
+    this.props.setEndDate(valueObject.value.max + '-12-31')
+    this.setState(valueObject)
   }
 
   render() {
@@ -17,7 +23,28 @@ export default class BasicSlider extends React.Component {
         maxValue={2017}
         minValue={1888}
         value={this.state.value}
-        onChange={value => this.setState({ value })} />
-    );
+        onChange={value => this.handleChange({ value })} />
+    )
   }
 }
+
+//========container for BasicSlider
+import { setStartSearchDate, setEndSearchDate } from '../reducers/movies'
+
+const mapStateToProps = (state, ownProps) => (
+  {
+    startSearchDate: state.movies.startSearchDate,
+    endSearchDate: state.movies.endSearchDate
+  }
+)
+
+const mapDispatchToProps = (dispatch) => ({
+  setStartDate: (startDate) => {
+    dispatch(setStartSearchDate(startDate))
+  },
+  setEndDate: (endDate) => {
+    dispatch(setEndSearchDate(endDate))
+  }
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(BasicSlider)
